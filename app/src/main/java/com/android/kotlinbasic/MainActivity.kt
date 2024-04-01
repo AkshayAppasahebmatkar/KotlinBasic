d163811d1508d13301e37f269e47a9003f86e9d5
Akshay <akshay@monocept.com>
2024-04-01 17:29:16 +0530

    Update java version 17 in project

--- app/src/main/java/com/android/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/android/kotlinbasic/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.android.kotlinbasic
 
-public class MainActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
 
+public class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
 }
