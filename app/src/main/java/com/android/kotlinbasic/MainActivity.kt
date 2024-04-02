0f08bf8b7fe3c30da0caea130378395292b4eb23
Akshay <akshay@monocept.com>
2024-04-02 15:52:11 +0530

    add new yml file

--- app/src/main/java/com/android/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/android/kotlinbasic/MainActivity.kt
@@ -2,12 +2,14 @@ package com.android.kotlinbasic
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.ListView
 import androidx.activity.ComponentActivity
 
 public class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
     }
-}
+    }
