e3e64dae57b1088043d4a0cb6ac2ecf74fa2aaab
Akshay <akshay@monocept.com>
2024-04-03 12:00:32 +0530

    Add New Firebase Distribution

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.icicilombard.phoenix"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
